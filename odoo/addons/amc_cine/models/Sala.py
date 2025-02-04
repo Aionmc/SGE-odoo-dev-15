8b25ae65bc5fa453da0e03154eb01c30cd75d558
Aionmc <correoInventado@gmail.com>
2025-02-04 14:14:27 +0100

    Creación de las primeras clases

---
+++ odoo/addons/amc_cine/models/Sala.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class Sala(models.Model):
+    _name = 'amc_cine.sala'
+    _description = 'Sala'
+    
+    numero = fields.Integer('Número', required=True, help='Introduzca el número de la sala')
+    filas = fields.Integer('Filas', help='Introduzca el número de filas que tiene la sala')
+    butacas = fields.Integer('Butacas', help='Introduzca el número de butacas que tiene la sala')
+    inclusividad = fields.Boolean('Inclusividad', help='Marque si esta sala tiene medidas para discapacitados')
